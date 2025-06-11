80ae2f6f2dc507305090f8b854558a1f1986925c
Tomomi <torano.manji@gmail.com>
2025-06-11 17:22:39 +0900

    Add / Posts setting

---
+++ src/content.config.ts
@@ -0,0 +1,22 @@
+import { defineCollection, z } from 'astro:content';
+
+const postsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    author: z.string(),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    tags: z.array(z.string().optional()),
+  }),
+});
+
+export const collections = {
+  posts: postsCollection,
+};
