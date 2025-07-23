7be2d4fb21b87ecb3db7a76926d0e465418d576b
Akiya <autumnsky64@gmail.com>
2025-07-23 09:21:40 +0900

    Use arrow expression

--- src/lib/sortedPosts.ts
+++ src/lib/sortedPosts.ts
@@ -1,6 +1,6 @@
 import { getCollection, type CollectionEntry } from 'astro:content';
 
-export async function getSortedPosts(): Promise<CollectionEntry<'posts'>[]>{
+export const getSortedPosts = async (): Promise<CollectionEntry<'posts'>[]> => {
   const posts = await getCollection('posts');
   return posts.sort(( a, b ) => {
      return new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime();
