2fafc8cf158652f2cbab2f8938b9c8a3a84c3a7b
Akiya <autumnsky64@gmail.com>
2025-07-29 11:50:59 +0900

    Show only publish post in production env

--- src/libs/sortedPosts.ts
+++ src/libs/sortedPosts.ts
@@ -1,7 +1,10 @@
 import { getCollection, type CollectionEntry } from 'astro:content';
 
 export const getSortedPosts = async (): Promise<CollectionEntry<'posts'>[]> => {
-  const posts = await getCollection('posts');
+  const posts = await getCollection( 'posts', ({ data }) => {
+    return import.meta.env.PROD ? data.status === 'publish' : true
+  });
+
   return posts.sort(( a, b ) => {
      return new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime();
   });
