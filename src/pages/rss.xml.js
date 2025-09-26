2d6107e59ddde1acc23f7d7857cccf497487e315
Tomomi <torano.manji@gmail.com>
2025-09-26 14:54:01 +0900

    Remove / _template folder and rss.xml.js in text

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,11 +2,6 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import { siteConfig } from '../constant';
 
 export async function GET(context) {
-  // __で始まる.mdファイルを除外
-  const allMdFiles = Object.fromEntries(
-    Object.entries(import.meta.glob('./**/*.md')).filter(([path]) => !/\/__[^/]*\.md$/.test(path)),
-  );
-
   return rss({
     title: siteConfig.title,
     description: siteConfig.description,
