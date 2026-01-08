fe8d7397f842c5e7d5ab3e43ad696d8c44bef908
Akiya <autumnsky64@gmail.com>
2026-01-08 21:11:25 +0900

    Add React for astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
-
+import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
 
   vite: {
     plugins: [tailwindcss()]
