a4afce07cdc4210aab496c20a6d517a23efb21e3
Akiya <autumnsky64@gmail.com>
2026-01-21 13:25:40 +0900

    Fix deprecate

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, } from 'astro:content';
+import { z } from 'astro/zod';
 
 // Post Collection
 // https://docs.astro.build/en/guides/content-collections
