4e7980fd2a579518a44528e612dd97cd6e49e0a9
Tomomi <torano.manji@gmail.com>
2026-01-22 10:03:09 +0900

    Wip / xml display modify

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,20 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 import { siteConfig } from '../constant';
 
 export async function GET(context) {
+  const posts = await getCollection('posts');
+
   return rss({
     title: siteConfig.title,
     description: siteConfig.description,
-    site: context.site,
-    items: await pagesGlobToRssItems(allMdFiles),
+    site: siteConfig.url,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/post/${post.id}/`,
+    })),
     customData: `<language>ja</language>`,
   });
 }
