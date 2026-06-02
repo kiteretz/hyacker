2945d20a0e2cd8f915bd991b7f0dee4f7285c0cc
Tomomi <torano.manji@gmail.com>
2026-06-02 13:48:20 +0900

    Refactor / SearchResults add aria-live

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,18 +1,18 @@
-import Card from '@components/Card'
-import { pageFindAtom, resultsAtom } from '@libs/jotai'
-import { useAtom, useAtomValue } from 'jotai'
-import { type FC, useEffect  } from 'react'
+import Card from '@components/Card';
+import { pageFindAtom, resultsAtom } from '@libs/jotai';
+import { useAtom, useAtomValue } from 'jotai';
+import { type FC, useEffect } from 'react';
 import dummyResult from '@libs/dummyResult';
 
-const SearchResults:FC = () => {
-  const [results,setResults] = useAtom(resultsAtom)
-  const pagefind = useAtomValue(pageFindAtom)
+const SearchResults: FC = () => {
+  const [results, setResults] = useAtom(resultsAtom);
+  const pagefind = useAtomValue(pageFindAtom);
 
-  useEffect(()=>{
+  useEffect(() => {
     const find = async () => {
-      const query = new URLSearchParams(document.location.search || '')
-      const searchWord = query.get("keyword") || ''
-      if( import.meta.env.DEV && searchWord ) {
+      const query = new URLSearchParams(document.location.search || '');
+      const searchWord = query.get('keyword') || '';
+      if (import.meta.env.DEV && searchWord) {
         setResults(dummyResult());
       } else {
         const search = await pagefind.search(searchWord);
@@ -22,31 +22,22 @@ const SearchResults:FC = () => {
             return {
               href: data.url,
               title: data.meta.title,
-              date: data.meta.pubDate
+              date: data.meta.pubDate,
             };
           }),
         );
-        setResults(results)
+        setResults(results);
       }
-    }
+    };
 
-    find()
+    find();
+  }, []);
 
-  }, [])
-
-  return results.length === 0
-    ? (
-    <div>
-      該当する記事はありません
+  return (
+    <div aria-live="polite">
+      {results.length === 0 ? <p>該当する記事はありません</p> : results.map((result) => <Card {...result} />)}
     </div>
-    ) : (
-    <div>
-      { results.map((result)=>{
-          return <Card {...result}/>
-        })
-      }
-    </div>
-  )
-}
+  );
+};
 
-export default SearchResults
+export default SearchResults;
