3b55829dc361674e4abe400a1edab255b6fe4f0a
Tomomi <torano.manji@gmail.com>
2026-06-02 13:18:59 +0900

    Add / Card component :forcus-visible

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -18,7 +18,13 @@ export type Card = {
 
 const Card: FC<Card> = ({ href, title, date, tags, img }) => {
   return (
-    <a href={href} className={twMerge('mb-px ml-px grid min-h-367 grid-rows-[auto_auto_auto_1fr] bg-white p-8')}>
+    <a
+      href={href}
+      className={twMerge(
+        'mb-px ml-px grid min-h-367 grid-rows-[auto_auto_auto_1fr] bg-white p-8',
+        'focus-visible:ring-2 focus-visible:ring-fg-800 focus-visible:outline-none focus-visible:ring-inset',
+      )}
+    >
       <h3 className="mb-8 px-8 text-18 font-semibold">{title}</h3>
       <p className="px-8 font-space-grotesk text-14 text-fg-400">{formatDate(date)}</p>
       <ul className="flex flex-wrap px-8 font-inter text-14 text-fg-400">
