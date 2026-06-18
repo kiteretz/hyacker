388033523b917251d80a6d26c22d7c2954bd3dca
Akiya <autumnsky64@gmail.com>
2026-06-18 15:57:32 +0900

    Fix disabled input style
    # Conflicts:
    #	src/components/Header.astro
    #	src/components/SearchInput.tsx

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -46,7 +46,6 @@ const SearchInput: FC<Props> = ({ className }) => {
       className={twMerge(
         'size-full h-[stretch] px-16 py-4',
         'placeholder:font-inter placeholder:text-16 placeholder:leading-none placeholder:font-medium placeholder:text-neutral-400',
-        'disabled:bg-gray-200',
         '[&::-webkit-search-cancel-button]:hidden',
         className,
       )}
