5b0919033f7cac1543531e07bbda1a7b4fdc94f6
Tomomi <torano.manji@gmail.com>
2026-06-22 17:40:12 +0900

    Fixed / optional chaining for nullable tags in postsByTag, sort imports in libs

--- src/libs/postsByTag.ts
+++ src/libs/postsByTag.ts
@@ -1,25 +1,25 @@
 import type { CollectionEntry } from 'astro:content';
+
 import { getSortedPosts } from './sortedPosts';
 
 type tag = {
-  name: string,
-  posts: CollectionEntry<'posts'>[]
-}
+  name: string;
+  posts: CollectionEntry<'posts'>[];
+};
 
 export const getPostsByTag = async (): Promise<tag[]> => {
   const posts = await getSortedPosts();
-  const uniqTags = new Set<string>( posts
-    .flatMap( ({data}) => data.tags )
-    .filter( (tag): tag is string => typeof tag === 'string' )
+  const uniqTags = new Set<string>(
+    posts.flatMap(({ data }) => data.tags).filter((tag): tag is string => typeof tag === 'string'),
   );
 
   const tags: tag[] = [];
-  uniqTags.forEach( (tag) => {
-    tags.push( {
+  uniqTags.forEach((tag) => {
+    tags.push({
       name: tag,
-      posts: posts.filter( ({data}) => data.tags.includes(tag) )
-    }
-  )})
+      posts: posts.filter(({ data }) => data.tags?.includes(tag)),
+    });
+  });
 
-  return tags.sort((a,b)=> b.posts.length - a.posts.length );
-}
+  return tags.sort((a, b) => b.posts.length - a.posts.length);
+};
