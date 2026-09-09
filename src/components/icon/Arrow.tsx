c30d1992c2da9f94cadbc4668bfc1537cc731cd6
Tomomi <torano.manji@gmail.com>
2026-09-09 11:21:28 +0900

    Fixed / Card back style modify and rotate style

---
+++ src/components/icon/Arrow.tsx
@@ -0,0 +1,20 @@
+import { twMerge } from '@libs/twMerge';
+
+import type { FC } from 'react';
+
+type Props = {
+  className?: string;
+};
+
+const Arrow: FC<Props> = ({ className }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    className={twMerge('aspect-square', className)}
+  >
+    <path stroke="currentColor" stroke-linejoin="bevel" stroke-width="2" d="M4 12h16m-8-8 8 8-8 8"></path>
+  </svg>
+);
+
+export default Arrow;
